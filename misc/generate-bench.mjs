1736455e7ecba25175fe308460ac15e12c328cb5
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-11-30 08:52:21 +0900

    feat: generate routes/many-components.tsx

--- misc/generate-bench.mjs
+++ misc/generate-bench.mjs
@@ -7,15 +7,37 @@ import * as fs from "node:fs";
 
 async function main() {
   const count = 100;
-  const content = await fs.promises.readFile(
+  const routeSource = await fs.promises.readFile(
     "misc/kentcdodds-blog.tsx",
     "utf-8"
   );
+
+  let manyCompoentSourcePart1 = "";
+  let manyCompoentSourcePart2 = "";
   for (let i = 0; i < count; i++) {
     const name = String(i).padStart(4, "0");
-    const filepath = `app/routes/page${name}.tsx`;
-    await fs.promises.writeFile(filepath, content);
+    await fs.promises.writeFile(`app/routes/page${name}.tsx`, routeSource);
+    await fs.promises.writeFile(
+      `app/components/component${name}.tsx`,
+      `
+        export default function Component() {
+          return <div>Component${name}</div>
+        }
+      `
+    );
+    manyCompoentSourcePart1 += `import Component${name} from "../components/component${name}";\n`;
+    manyCompoentSourcePart2 += `<li><Component${name} /></li>\n`;
   }
+
+  await fs.promises.writeFile(
+    `app/routes/many-components.tsx`,
+    manyCompoentSourcePart1 +
+      `
+    export default function ManyComponentsRoute() {
+      return <ul>${manyCompoentSourcePart2}</ul>
+    }
+  `
+  );
 }
 
 main();
